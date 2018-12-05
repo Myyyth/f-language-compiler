b82c9bef61d7fa75dd9d7b321bf83fdda905e6ce
Myyyth <bas9339@gmail.com>
2018-12-05 14:40:26 +0300

    Template

---
+++ src/main/java/bytecode_generator/Mapper.java
@@ -0,0 +1,28 @@
+package bytecode_generator;
+
+import org.objectweb.asm.*;
+import static org.objectweb.asm.Opcodes.*;
+
+public class Mapper {
+    public String createComplexClass() {
+        return "public class Complex {\n" +
+                "        public double real;\n" +
+                "        public double imaginary;\n" +
+                "\n" +
+                "        public Complex() { }\n" +
+                "\n" +
+                "        public Complex(double real, double imaginary) {\n" +
+                "            this.real = real;\n" +
+                "            this.imaginary = imaginary;\n" +
+                "        }\n" +
+                "    }\n" +
+                "@Override\n" +
+                "        public String toString() {\n" +
+                "            return String.valueOf(real) + \"i\" + String.valueOf(imaginary);\n" +
+                "        }\n";
+    }
+
+    public String createComplexObject(double real, double imaginary) {
+
+    }
+}
